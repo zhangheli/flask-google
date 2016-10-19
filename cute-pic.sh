a96bb50f57c7164f20d781a1d291680fae11eba8
zhangheli <ibm1989@yeah.net>
2016-10-20 02:16:45 +0800

    下载图片
    下载图片到本地

---
+++ cute-pic.sh
@@ -0,0 +1,4 @@
+K=`curl -s http://www.symmz.com/sitemap_baidu.xml | ack -o 'http://www.symmz.com/\w+/\d+-\d+.html' | head -n 5`
+for item in `echo $K`;do
+	ack -o 'http://img\S+/\d+max.jpg' html/`cd html && curl -s "$item" -O -w '%{filename_effective}' ` | xargs -L 1 bash -cx  'cd jpg && wget -c $0'
+done 
